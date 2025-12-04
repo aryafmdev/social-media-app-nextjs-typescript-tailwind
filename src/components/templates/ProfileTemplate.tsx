7de984b85355a85f7b745886738502420fb6572c
aryafmdev <muhammadaryafm@gmail.com>
2025-12-04 10:36:20 +0700

    fix: minor fixed on profile template page

--- src/components/templates/ProfileTemplate.tsx
+++ src/components/templates/ProfileTemplate.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function ProfileTemplate({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen w-full bg-neutral-950">
-      <div className="max-w-md mx-auto px-7xl py-7xl">{children}</div>
+    <div className="min-h-screen w-full bg-black">
+      <div className="mx-auto px-7xl py-7xl">{children}</div>
     </div>
   );
 }
